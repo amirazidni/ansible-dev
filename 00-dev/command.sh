52443ffb855110fe78633c8da373c76500a0a404
amirazidni <zidni.imani@gmail.com>
2023-08-02 05:03:30 +0700

    Install nginx with tls in ubuntu

--- 00-dev/command.sh
+++ 00-dev/command.sh
@@ -11,3 +11,11 @@ ansible localvm -m ping -i hosts
 
 # to run playbook.yml
 ansible-playbook -i inventory.yml playbook.yml 
+
+# create self-signed key and certificate
+sudo openssl req -x509 -nodes -days 365 \
+ -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key \
+ -out /etc/ssl/certs/nginx-selfsigned.crt
+
+ # to run nginx-tls.yml
+ansible-playbook -i inventory.yml nginx-tls.yml
